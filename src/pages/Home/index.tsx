e5e56f16079d2afd74544e64533bda66994d5c56
Matheus Schneider <matheus.ssm@gmail.com>
2021-03-24 16:19:56 -0300

    challenge complete!

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,11 +23,14 @@ interface CartItemsAmount {
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const sumAmountCopy = { ...sumAmount };
+
+    sumAmountCopy[product.id] = product.amount;
+    return sumAmountCopy;
+  }, {} as CartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
@@ -41,7 +44,7 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
   return (
@@ -59,7 +62,7 @@ const Home = (): JSX.Element => {
             >
               <div data-testid='cart-product-quantity'>
                 <MdAddShoppingCart size={16} color='#FFF' />
-                {/* {cartItemsAmount[product.id] || 0} */} 2
+                {cartItemsAmount[product.id] || 0}
               </div>
 
               <span>ADICIONAR AO CARRINHO</span>
